14115367f5e4c202f9b7e6c7f63330ee427d1a77
mnjammy007 <naseem4954r@gmail.com>
2024-08-04 18:51:37 +0530

    module 9 done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date")
+    list_display = ("title", "date", "author")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("caption",)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag, TagAdmin)
